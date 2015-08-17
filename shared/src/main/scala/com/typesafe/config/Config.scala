ed6374b720507c8284111f90895f52ac1c59a99b
Andrea <andrea.peruffo1982@gmail.com>
2015-08-17 15:55:44 +0200

    null instead of exception

--- shared/src/main/scala/com/typesafe/config/Config.scala
+++ shared/src/main/scala/com/typesafe/config/Config.scala
@@ -27,7 +27,7 @@ case class Config(cfg: shocon.Config.Value) {
   import shocon.Extractors._
 
   val fallback = Promise[shocon.Config.Value]
-  
+
   def this() = {
     this(shocon.Config("{}"))
   }
@@ -35,7 +35,7 @@ case class Config(cfg: shocon.Config.Value) {
   def root() = {
     new ConfigObject {
       val inner = self.cfg
-      def unwrapped = 
+      def unwrapped =
         cfg.as[shocon.Config.Object].get.fields.map{
           case (k, v) => (k -> v.toString)
         }.asJava
@@ -45,42 +45,54 @@ case class Config(cfg: shocon.Config.Value) {
           }.asJava.entrySet
     }
   }
-  
+
   def withFallback(c: Config) = {
     fallback.success(c.cfg)
     this
   }
 
-  def getOrThrow[T](path: String)(implicit ev: Extractor[T]) =
-    cfg.get(path) 
+  def getOrThrow[T](path: String)(implicit ev: Extractor[T]): T =
+    cfg.get(path)
        .flatMap(_.as[T](ev))
-       .getOrElse( throw ConfigException.Missing(path) )
-  
+       .getOrElse{
+         fallback.future.value match {
+            case Some(Success(flbCfg)) =>
+              new Config(flbCfg).getOrThrow[T](path)(ev)
+            case _ =>
+              //println("Config Exception cannot extract "+path+" from "+cfg)
+              null.asInstanceOf[T] //throw ConfigException.Missing(path)
+         }
+      }
+
   def hasPath(path: String)     = cfg.get(path).isDefined
-  
+
   def getConfig(path: String)   = new Config(getOrThrow[shocon.Config.Value](path))
-  
+
   def getString(path: String)   = getOrThrow[String](path)
-  
+
   def getBoolean(path: String)  = getOrThrow[Boolean](path)
-  
+
   def getInt(path: String)      = getOrThrow[Int](path)
 
   def getDouble(path: String)   = getOrThrow[Double](path)
-    
+
   def getStringList(path: String) = getOrThrow[ju.List[String]](path)
 
   private val millis = Set("ms", "millis", "milliseconds")
   private val nanos = Set("ns", "nanos", "nanoseconds")
   def getMillisDuration(path: String) = {
-    val res = getString(path)
-    val parts = res.split("[ \t]")
-    // either parts(1) is empty string (fallback: ms) 
-    // or it actually is one spelling for "milliseconds" 
-    assert( parts.size == 1 || (millis contains parts(1)) )
-    Duration(parts(0).toInt, MILLISECONDS)
+    try {
+      val res = getString(path)
+      val parts = res.split("[ \t]")
+      // either parts(1) is empty string (fallback: ms)
+      // or it actually is one spelling for "milliseconds"
+      assert( parts.size == 1 || (millis contains parts(1)) )
+      Duration(parts(0).toInt, MILLISECONDS)
+    } catch {
+      case err: Exception => null
+    }
   }
-  
+
   def getNanosDuration(path: String) = {
     val res = getString(path)
     val parts = res.split("[ \t]")
